f88f9164545f901f5bf1b99976108ca7c646dd64
cana927 <limjulia927@gmail.com>
2022-07-08 18:17:41 -0700

    finished 1617jrc1 acsl

--- ACSL/src/_1617jrc1.java
+++ ACSL/src/_1617jrc1.java
@@ -27,22 +27,48 @@ public class _1617jrc1 {
 		
 		String all = sc.nextLine();
 		all = all.replace(", ","");
-		System.out.println (all);
 		
 		oppnum = all.charAt(0) - '0';
 		oppshape = "" + all.charAt(1);
 		
-		for (int i=2; i<all.length(); i=i+2) {
-			nums [i/2-1] = all.charAt(i);
+		for (int i=2; i<all.length()-1; i=i+2) {
+			nums [i/2-1] = all.charAt(i) - '0';
 			shapes [i/2-1] = "" + all.charAt(i+1);
 		}
-	//problem?
+		
 	}
 	
 	public static void solve() {
+		int isSuit = 0;
+		int lowest = 20;
+		int lowesthigher = 20;
+		for (int i = 0; i<5; i++) {
+			if (shapes[i].equals (oppshape)) {
+				isSuit++;
+				if (nums[i] < lowest) {
+					lowest = nums[i];
+				}
+				if ( nums[i] < lowesthigher && nums[i] > oppnum) {
+					lowesthigher = nums[i];
+				}
+			}
+		}
 		
+		if (isSuit>0) {
+			if (lowesthigher != 20) {
+				output += lowesthigher;
+			}
+			else {
+				output += lowest;
+			}
+			output = output + ", " + oppshape;
+
+		} else {
+			output = "NONE";
+		}
 	}
+	
 	public static void output () {
-		
+		System.out.println (output);
 	}
 }
