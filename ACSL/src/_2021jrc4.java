e4a3885ddd237c8fb26212ece387d82a4e397e89
cana927 <limjulia927@gmail.com>
2022-08-28 18:17:27 -0700

    finished 2021jrc4 acsl

--- ACSL/src/_2021jrc4.java
+++ ACSL/src/_2021jrc4.java
@@ -1,4 +1,18 @@
 import java.util.*;
+/*
+2 12 13 23 31 34 41
+1 12 23 34 11 21 32 45 53 95 43 99 29 91
+3 12 23 34 41 31 52 45 61 14 21 33 55 13 54 32 56 36
+1 12 11 33 34 43 55 52 41 31 25 88 79 98 45 13 42 87 35 51 21 14 78
+2 12 11 33 34 43 55 52 41 31 25 88 79 98 45 13 42 87 35 51 21 14 78
+
+1 12 31 41 42 43 45 51 63 64 56 16
+2 12 13 22 23 24 34 42 98 71 87 17 96 67
+3 12 14 21 24 25 32 41 43 59 65 91 87 76 95
+2 11 12 14 15 23 25 31 43 45 51 52 68 79 87 89
+3 55 77 45 54
+
+ */
 
 public class _2021jrc4 {
 	static int query;
@@ -23,8 +37,8 @@ public class _2021jrc4 {
 		StringTokenizer st = new StringTokenizer(sc.nextLine());
 		while(st.hasMoreTokens()) {
 			String t = st.nextToken();
-			int origin = t.charAt(0) - '0';
-			int dest = t.charAt(1) - '0';
+			int origin = t.charAt(0) - '0' -1;
+			int dest = t.charAt(1) - '0' -1;
 			adj[origin][dest] = 1;
 		}
 	}
@@ -61,14 +75,31 @@ public class _2021jrc4 {
 				}
 				
 			}
-			//find sum
-			output = 
+			int sum =0;
+			for (int i =0; i<9; i++) {
+				if (adj[vertex][i] == 1) {
+					sum += (vertex+1)* 10 + i + 1;
+				}
+			}
+			output = sum;
 		}
 		else {
-			
+			int sum = 0;
+			for (int i= 0; i<9; i++) {
+				for (int j = 0; j<9; j++) {
+					if (adj[i][j] ==1) {
+						for (int r =0; r<9; r++) { 
+							if (adj[j][r] == 1) {
+								sum++;
+							}
+						}
+					}
+				}
+			}
+			output = sum;
 		}
 	}
 	public static void output() {
-		
+		System.out.println (output);
 	}
 }
